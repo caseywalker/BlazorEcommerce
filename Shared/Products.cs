638138c04655828611cef69478906935765fdcfd
Walker, Casey <casey.walker@ingramcontent.com>
2023-01-11 12:02:20 -0600

    Add project files.

---
+++ Shared/Products.cs
@@ -0,0 +1,19 @@
+﻿global using BlazorEcommerce.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+    }
+}
