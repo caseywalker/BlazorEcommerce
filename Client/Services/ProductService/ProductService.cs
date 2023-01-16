8083d5445983b560d3fadc6916bed062745d2e4a
Caseywalker <casey.walker118@gmail.com>
2023-01-16 10:13:34 -0600

    Adding product services for client.

---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductService
+{
+	public class ProductService : IProductService
+	{
+		private readonly HttpClient _client;
+
+		public ProductService(HttpClient client)
+		{
+			_client = client;
+		}
+		public List<Product> Products { get; set; } = new List<Product>();
+
+		public async Task GetProducts()
+		{
+			var result = await _client.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+			if (result != null && result.Data!= null)
+			{
+				Products = result.Data;
+			}
+		}
+	}
+}
