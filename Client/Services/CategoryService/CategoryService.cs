7a867dd19a708e75810aec6dfa7283263b2aa2c2
Caseywalker <casey.walker118@gmail.com>
2023-02-10 14:01:20 -0600

    Adding products by category to Client

--- Client/Services/CategoryService/CategoryService.cs
+++ Client/Services/CategoryService/CategoryService.cs
@@ -2,5 +2,21 @@
 {
     public class CategoryService : ICategoryService
     {
+        private readonly HttpClient _client;
+
+        public CategoryService(HttpClient client)
+        {
+            _client = client;
+        }
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public async Task GetCategories()
+        {
+            var response = await _client.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
+            if(response != null && response.Data != null)
+            {
+            Categories = response.Data;
+            }
+        }
     }
 }
