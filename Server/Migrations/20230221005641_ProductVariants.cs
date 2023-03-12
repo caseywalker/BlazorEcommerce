1ea74a2e612f81714001aa1af408c6990c02faf5
Caseywalker <casey.walker118@gmail.com>
2023-03-12 12:03:01 -0500

    Adding product variants to EF Core

---
+++ Server/Migrations/20230221005641_ProductVariants.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductVariants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductVariants",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductVariants", x => new { x.ProductId, x.ProductTypeId });
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_ProductTypes_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Default" },
+                    { 2, "Paperback" },
+                    { 3, "E-Book" },
+                    { 4, "Audiobook" },
+                    { 5, "Stream" },
+                    { 6, "Blu-ray" },
+                    { 7, "VHS" },
+                    { 8, "PC" },
+                    { 9, "PlayStation" },
+                    { 10, "Xbox" },
+                    { 11, "Dog food" },
+                    { 12, "Hockey Equipment" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductVariants",
+                columns: new[] { "ProductId", "ProductTypeId", "OriginalPrice", "Price" },
+                values: new object[,]
+                {
+                    { 1, 8, 19.99m, 9.99m },
+                    { 1, 9, 0m, 7.99m },
+                    { 1, 10, 29.99m, 19.99m },
+                    { 2, 6, 14.99m, 7.99m },
+                    { 3, 2, 0m, 6.99m },
+                    { 4, 12, 0m, 139.99m },
+                    { 5, 11, 0m, 3.99m },
+                    { 6, 7, 0m, 2.99m },
+                    { 7, 6, 0m, 69.99m },
+                    { 7, 7, 29.99m, 19.99m },
+                    { 8, 4, 24.99m, 9.99m },
+                    { 9, 5, 0m, 14.99m },
+                    { 10, 1, 299m, 159.99m },
+                    { 11, 7, 399m, 79.99m },
+                    { 12, 8, 40.99m, 19.99m },
+                    { 12, 9, 40.99m, 29.99m },
+                    { 12, 10, 40.99m, 39.99m },
+                    { 13, 8, 40.99m, 39.99m },
+                    { 14, 10, 40.99m, 39.99m },
+                    { 15, 10, 40.99m, 39.99m },
+                    { 16, 1, 40.99m, 139.99m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariants_ProductTypeId",
+                table: "ProductVariants",
+                column: "ProductTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductVariants");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 59.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 19.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 9.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 99.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Price",
+                value: 39.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Price",
+                value: 9.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Price",
+                value: 7.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Price",
+                value: 6.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 4.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Price",
+                value: 3.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Price",
+                value: 2.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Price",
+                value: 49.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Price",
+                value: 9.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Price",
+                value: 14.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Price",
+                value: 159.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Price",
+                value: 79.99m);
+        }
+    }
+}
